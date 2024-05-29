343967c6fd0ea33ae77ad6291d100cf0b8578b00
spd <384220729@qq.com>
2024-05-29 14:51:01 +0800

    Update new function.

--- R/function.R
+++ R/function.R
@@ -58,6 +58,7 @@ get_chrDfList <- function(ndata, m){
           return(peakDf)
         }
       }))
+      chrDf_i$smooth_int <- smoothMean(chrDf_i$intensity, size = 3)
       return(chrDf_i)
     }
     chrDfList_ms2 <- lapply(1:nrow(mfeatureTable_ms2), function(i) {
@@ -81,21 +82,80 @@ get_chrDfList <- function(ndata, m){
       return(peakDf)
     }
   }))
+  chrDf_ms1$smooth_int <- smoothMean(chrDf_ms1$intensity, size = 3)
   chrDfList_ms1 <- list(chrDf_ms1)
   names(chrDfList_ms1) <- mfeatureTable_ms1$feature_id
   chrDfList <- list(ms1 = chrDfList_ms1, ms2 = chrDfList_ms2)
   return(chrDfList)
 }
-#' @title plot_chrDfList
+#' @title smoothMean
+#' @description
+#' Smooth function using mean.
 #'
-#' @param chrDfList
+#' @param int intensity.
+#' @param size mean size.
 #'
-#' @return
+#' @return intensity vector
 #' @export
 #'
 #' @examples
+#' intensity <- c(1701.196, 2121.159, 2401.849, 2043.653, 1542.964,  723.803)
+#' smoothMean(int = intensity)
+smoothMean <- function(int, size = 3){
+  if(size %% 2 == 0){
+    stop("size should be a singular!")
+  }
+  half_size <- floor(size / 2)
+  N <- length(int)
+  special_idx <- c(seq_len(half_size), rev((N:(N - half_size + 1))))
+  smooth_int <- sapply(1:N, function(i) {
+    if(i %in% special_idx){
+      return(int[i])
+    }
+    y <- sapply((i - half_size):(i + half_size), function(j){
+      int[j]
+    })
+    smooth_int <- mean(y)
+  })
+  return(smooth_int)
+}
+#' @title plot_chrDfList
+#' @description
+#' plot_chrDfList
+#'
+#' @param chrDfList chrDfList
+#'
+#' @return ggplot object
+#' @export
+#'
+#' @examples
+#' plot_chrDfList(chrDfList = mchrDfList)
 plot_chrDfList <- function(chrDfList){
-
+  chrDfList_ms1 <- chrDfList$ms1
+  feature_ms1_name <- names(chrDfList_ms1)
+  chrDf_ms1 <- chrDfList_ms1[[1]]
+  chrDfList_ms2 <- chrDfList$ms2
+  dfList <- lapply(1:length(chrDfList_ms2), function(i) {
+    df_tmp <- chrDfList_ms2[[i]]
+    df_tmp$level <- "2"
+    df_tmp$feature <- i
+    return(df_tmp)
+  })
+  df_tmp <- chrDf_ms1
+  df_tmp$level <- "1"
+  df_tmp$feature <- length(dfList) + 1
+  dfList <- append(dfList, list(df_tmp))
+  df <- purrr::list_rbind(dfList)
+  group <- unique(df$level)
+  group_number <- length(group)
+  group_color <- RColorBrewer::brewer.pal(6, "Set1")[1:group_number]
+  names(group_color) <- group
+  p <- ggplot2::ggplot(data = df, mapping = ggplot2::aes(x = rt, group = feature, color = level)) +
+    ggplot2::geom_line(mapping = ggplot2::aes(y = smooth_int), linewidth = 1) +
+    ggplot2::theme_bw() +
+    ggplot2::labs(x = "Retention Time", y = "Intensity") +
+    ggplot2::scale_color_manual(values = group_color)
+  return(p)
 }
 
 
